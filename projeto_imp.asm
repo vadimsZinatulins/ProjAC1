98450a58dd94462c1ba9ca07b6fbcc0ae128714c
vadimsZinatulins <vadims.zinatulins@pt.softinsa.com>
2020-10-20 04:52:56 +0100

    Huge refactor

--- projeto_imp.asm
+++ projeto_imp.asm
@@ -9,10 +9,10 @@
     div_msg_a               db  'Introduza o dividendo', 0ah, 0dh, '$'
     div_msg_b               db  'Introduza o divisor', 0ah, 0dh, '$'
     
-    div_input_a_str         db  6, 0, 6 dup(?)
+    div_input_a_str         db  6, 0, 7 dup(?)
     div_input_a_value       dw  0
      
-    div_input_b_str         db  6, 0, 6 dup(?)
+    div_input_b_str         db  6, 0, 7 dup(?)
     div_input_b_value       dw  0
                                                   
     div_msg_result_pt1      db  ' a dividir por $'
@@ -22,8 +22,11 @@
     ; SQRT algorithm variables
     sqrt_msg                db  'Introduza o numero', 0ah, 0dh, '$'
     
-    sqrt_input_str          db  6, 0 6 dup(?)
+    sqrt_input_str          db  6, 0 7 dup(?)
     sqrt_input_value        dw  0
+    
+    sqrt_msg_result_pt1     db  'Raiz quadrada de $'
+    sqrt_msg_result_pt2     db  ' ', 082h, ' $'
     
     ; General variables
     aux_var_a               dw  0, 0ffffh
@@ -245,10 +248,12 @@ GET_INPUT_STR_CHECK_OVERFLOW:
     jmp GET_INPUT_STR_END
 
 GET_INPUT_STR_OVERFLOW_ERROR:
+    ; Print overflow error
     lea dx, overflow_msg
     mov ah, 09h
     int 21h
     
+    ; Ask user for input again
     lea dx, introduce_again_msg
     mov ah, 09h
     int 21h
@@ -256,10 +261,12 @@ GET_INPUT_STR_OVERFLOW_ERROR:
     jmp GET_INPUT_STR_BEGIN
     
 GET_INPUT_STR_INVALID_INPUT:
+    ; Print that user introduced invalid input
     lea dx, invalid_input_msg
     mov ah, 09h
     int 21h
     
+    ; Ask user for input again
     lea dx, introduce_again_msg
     mov ah, 09h
     int 21h
@@ -293,37 +300,47 @@ Print_Number proc
     
     mov ax, bx      
     
+    ; If AX is zero, then make sure that atleast 0 is printed
     cmp ax, 00h
     jne PRINT_NUMBER_GET_DIGITS
+    ; Add 0 to the stack so it can be printed
     mov dx, 030h
     push dx
     
 PRINT_NUMBER_GET_DIGITS:
+    ; If AX is 0 then there are no more digits to print
     cmp ax, 00h
     je PRINT_NUMBER_READY_TO_PRINT
     
+    ; AX = AX / CX
     mov dx, 00h
-    
     div cx
     
+    ; If the remainder (DX) >= 10 then print a character instead of digit
     cmp dx, 0ah
     jae PRINT_NUMBER_IS_NOT_NUMBER
     
+    ; Add 30h to DX
     add dx, 30h
+    ; Add DX to the stack so it can be printed
     push dx
 
     jmp PRINT_NUMBER_GET_DIGITS
      
 PRINT_NUMBER_IS_NOT_NUMBER:
+    ; Add 41h to DX
     add dx, 41h
+    ; Add DX to the stack so it can be printed
     push dx   
     
     jmp PRINT_NUMBER_GET_DIGITS
     
 PRINT_NUMBER_READY_TO_PRINT:
+    ; If BP == SP then the stack is empty and there are no more digits to print
     cmp bp, sp
     je PRINT_NUMBER_FINISH
     
+    ; Pop digit from stack and print it
     pop dx
     mov ah, 02h
     int 21h
@@ -346,22 +363,26 @@ Print_Number endp
 ; Output:
 ;           None
 ; Description:
-;           
+;           Shifts the entire string at BX to the right and insert AL at the left of the string
 Push_To_Front proc
     pusha
     
+    ; DI = String
     mov di, bx
+    ; CX = String Size
     mov cx, 00h
     mov cl, [bx + 1]
-    mov bx, 00
-    mov ax, 00h
     
 PUSH_TO_FRONT_SHIFT:
+    ; BX = Index
     mov bx, cx
+    ; AH = String[Index]
     mov ah, [di + bx + 1]
+    ; String[Index + 1] = AH
     mov [di + bx + 2], ah
     loop PUSH_TO_FRONT_SHIFT    
     
+    ; String[0] = AL
     mov [di + 2], al
     
     popa
@@ -369,30 +390,131 @@ PUSH_TO_FRONT_SHIFT:
 Push_To_Front  endp
 
 ; Input:
+;           AX -> LS value
+;           DX -> MS value
+; Output:
+;           AX -> LS value
+;           DX -> MS value
+; Description:
+;           Multiplies DX:AX by 100
+Mul_By_100 proc
+    push ax
+    push dx
+    
+    mov bp, sp
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    add ax, [bp + 02h]
+    adc dx, [bp + 00h]
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    add ax, [bp + 02h]
+    adc dx, [bp + 00h]
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    add sp, 04h
+
+    ret
+Mul_By_100 endp 
+
+; Input:
+;           AX -> LS value
+;           DX -> MS value
+; Output:
+;           AX -> LS value
+;           DX -> MS value
+; Description:
+;           Multiplies DX:AX by 10
+Mul_By_10 proc
+    push ax
+    push dx
+    
+    mov bp, sp 
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    shl ax, 01h
+    rcl dx, 01h    
+    
+    add ax, [bp + 02h]
+    adc dx, [bp + 00h]
+    
+    shl ax, 01h
+    rcl dx, 01h
+    
+    add sp, 04h
+    
+    ret
+Mul_By_10 endp
+
+; Input:
+;           AX -> LS value of A
+;           BX -> LS value of B
+;           CX -> MS value of B
+;           DX -> MS value of A
+; Output:
+;           AX -> If DX:AX > CX:DX then AX = 1. AX = 0 otherwise.
+; Description:
+;           Checks if DX:AX is above CX:BX
+Check_Is_Above_32 proc
+    cmp dx, cx
+    ja CHECK_IS_ABOVE_32_IS_ABOVE
+    jb CHECK_IS_ABOVE_32_IS_BELOW
+    cmp ax, bx
+    ja CHECK_IS_ABOVE_32_IS_ABOVE
+    
+CHECK_IS_ABOVE_32_IS_BELOW:
+    mov ax, 00h
+    jmp CHECK_IS_ABOVE_32_END
+    
+CHECK_IS_ABOVE_32_IS_ABOVE:
+    mov ax, 01h
+    jmp CHECK_IS_ABOVE_32_END
+
+CHECK_IS_ABOVE_32_END:
+    ret
+Check_Is_Above_32 endp
+
+; Input:
+;           AX -> Index
 ;           BX -> String
-;           CX -> Index
 ; Output:
 ;           AX -> Hexadecimal value
 ; Description:
-;
+;           Returns hexadecimal value of the pair at index AX of BX String
 Get_Next_Word proc
     ; Store all registers state
     pusha
     
     ; Setup
-    mov di, bx
-    mov bx, cx
-    mov dx, 0ah
+    mov di, ax      ; DI = Index
+    mov dx, 0ah     ; DX = 10
     
-    ; AX = str[0] * 10
-    mov ax, 00h
-    mov al, [di + bx + 2]
-    mul dx
+    ; AX = String[0] x 10
+    mov ax, 00h             ; AX = 0
+    mov al, [di + bx + 2]   ; AX = String[index + 2]
+    mul dx                  ; AX = AX x DX
     
-    ; AX = AX + str[1]
-    mov dx, 00h
-    mov dl, [di + bx + 3]
-    add ax, dx
+    ; AX = AX + String[index + 3]
+    mov dx, 00h             ; DX = 0
+    mov dl, [di + bx + 3]   ; DX = String[index + 3]
+    add ax, dx              ; AX = AX + DX
     
     ; Store the final result in the stack
     push ax
@@ -583,14 +705,97 @@ RUN_DIVISION_ALG_FINISH:
     ret
 Run_Division_Alg endp
 
-Run_Sqrt_Alg proc
-    mov bp, sp
+; Input:
+;               aux_var_a -> Remainder
+;               aux_var_b -> Result
+; Output:
+;               AX -> LS value of the calculation
+;               BX -> Digit found
+;               DX -> MS value of the calculation
+; Description:
+;               Finds the first digit such that (((Result x 2) x 10) + DIGIT x DIGIT) > Remainder, and then returns
+;               the found digit -1. Also returns the result of the calculation in DX:AX
+Find_Sqrt_Next_Digit proc
+    push cx
+  
+    mov bx, 00h    
     
+FIND_SQRT_NEXT_DIGIT_LOOP:
+    inc bx
+    
+    ; DX:AX = Result
+    mov ax, aux_var_b[02h]
+    mov dx, aux_var_b[00h]
+    
+    ; DX:AX = DX:AX x 2
+    shl ax, 01h
+    rcl dx, 01h
+    
+    ; DX:AX = DX:AX x 10
+    call Mul_By_10
+    
+    ; DX:AX = DX:AX + BX
+    add ax, bx
+    adc dx, 00h
+    
+    ; DX:AX = DX:AX x BX
+    mul bx
+    
+    ; Store AX and BX
+    push ax
+    push bx
+    
+    ; CX:BX = Remainder
+    mov bx, aux_var_a[2]
+    mov cx, aux_var_a[0]
+                   
+    ; AX = DX:AX > CX:BX
+    call Check_Is_Above_32
+    
+    ; Store result in CX
+    mov cx, ax
+    
+    ; Restore BX and AX
+    pop bx     
+    pop ax
+    
+    ; If DX:AX > CX:BX then BX is our next digit +1
+    cmp cx, 01h
+    je FIND_SQRT_NEXT_DIGIT_FOUND 
+    
+    cmp bx, 09h
+    jbe FIND_SQRT_NEXT_DIGIT_LOOP
+    
+FIND_SQRT_NEXT_DIGIT_FOUND:
+    dec bx 
+    
+    ; DX:AX = Result
+    mov ax, aux_var_b[02h]
+    mov dx, aux_var_b[00h]
+    
+    ; DX:AX = DX:AX x 2
+    shl ax, 01h
+    rcl dx, 01h
+    
+    ; DX:AX = DX:AX x 10
+    call Mul_By_10
+    
+    ; DX:AX = DX:AX + BX
+    add ax, bx
+    adc dx, 00h
+    
+    ; DX:AX = DX:AX x BX
+    mul bx
+    
+    pop cx
+        
+    ret
+Find_Sqrt_Next_digit endp
+
+Run_Sqrt_Alg proc    
     lea dx, sqrt_msg
     mov ah, 09h
     int 21h
-    
-    push bp         ; Store BP value
                 
     lea ax, sqrt_input_str 
     push ax
@@ -604,53 +809,177 @@ Run_Sqrt_Alg proc
     
     add sp, 04h     ; Remove arguments from the stack
     
-    pop bp          ; Restore BP value
+    ; Check the number of digits in the input
+    ; If the number of digits is odd then add 00 and the first input value
     
+    ; AL = String Size
     mov ax, 00h
     mov al, sqrt_input_str[1]
     
+    ; BL = 2
     mov bx, 02h
-    
-    mov dx, 00h
-    
+ 
+    ; AX = AX / BL
+    mov dx, 00h   
     div bx
     
+    ; If DX == 0 then the input size is even
     cmp dx, 00h
     je RUN_SQRT_ALG_IS_EVEN_NUMBER
     
-    mov al, 00h
-    lea bx, sqrt_input_str
-    call Push_To_Front
+    mov al, 00h             ; Value to insert into the String
+    lea bx, sqrt_input_str  ; Target String
+    call Push_To_Front      ; Insert AL to the front of the String
+    inc sqrt_input_str[1]   ; Increment String Size
     
 RUN_SQRT_ALG_IS_EVEN_NUMBER:
                          
-    mov aux_var_a[0], 00h   ; Remainder LS
-    mov aux_var_a[1], 00h   ; Remainder MS
+    mov aux_var_a[0], 00h   ; Remainder MS
+    mov aux_var_a[2], 00h   ; Remainder LS
                                        
-    mov aux_var_b[0], 00h   ; Result LS
-    mov aux_var_b[1], 00h   ; Result MS
+    mov aux_var_b[0], 00h   ; Result MS
+    mov aux_var_b[2], 00h   ; Result LS
     
-    lea bx, sqrt_input_str
+    ; CX = (String Size) / 2    This is needed because we will traverse the
+    ;                           String in pairs
     mov cx, 00h
     mov cl, sqrt_input_str[1]
+    shr cx, 01h
     
-RUN_SQRT_ALG_INTEGER_PART:
-    push cx
-    
+RUN_SQRT_ALG_INTEGER_PART:               
+    ; BX = (String Size) / 2
     mov bx, 00h
     mov bl, sqrt_input_str[1]
+    shr bx, 01h
     
+    ; BX = (BX - CX) x 2
     sub bx, cx
-                            
+    shl bx, 01h             
                             ; Args:
-    mov cx, bx              ;   CX = Index
-    lea bx, sqrt_input_str  ;   BX = Str
-    call Get_Next_Word
+    mov ax, bx              ;   AX = Index of the pair
+    lea bx, sqrt_input_str  ;   BX = Input string
     
-    pop cx
+    call Get_Next_Word      ;   AX = hexadecimal value of the pair
+     
+    ; Store AX value
+    push ax
+          
+    ; DX:AX = Remainder
+    mov dx, aux_var_a[0]
+    mov ax, aux_var_a[2]
     
-    loop RUN_SQRT_ALG_INTEGER_PART
+    ; DX:AX = DX:AX x 100
+    call Mul_By_100
     
+    ; Remainder = DX:AX                    
+    mov aux_var_a[2], ax
+    mov aux_var_a[0], dx
+    
+    ; Restore AX value
+    pop ax
+    
+    ; Remainder = Remainder + AX
+    add aux_var_a[2], ax
+    adc aux_var_a[0], 00h
+    
+    call Find_Sqrt_Next_Digit     ; BX = Next algarism, DX:AX = Calculation value
+    
+    ; Remainder = Remainder - Result
+    sub aux_var_a[2], ax
+    sbb aux_var_a[0], dx
+    
+    ; DX:AX = Result
+    mov ax, aux_var_b[02h]
+    mov dx, aux_var_b[00h]
+    
+    ; DX:AX = DX:AX x 10
+    call Mul_By_10
+    
+    ; DX:AX = DX:AX + BX
+    add ax, bx
+    adc dx, 00h
+    
+    ; Result = DX:AX
+    mov aux_var_b[2], ax
+    mov aux_var_b[0], dx
+        
+    loop RUN_SQRT_ALG_INTEGER_PART   
+    
+    call Print_Line_Break                                      
+    
+    lea dx, sqrt_msg_result_pt1
+    mov ah, 09h
+    int 21h
+    
+    mov bx, sqrt_input_value
+    mov cx, 0ah
+    call Print_Number
+    
+    lea dx, sqrt_msg_result_pt2
+    mov ah, 09h
+    int 21h
+    
+    mov bx, aux_var_b[0]
+    mov cx, 0ah
+    call Print_Number 
+    
+    mov bx, aux_var_b[2]
+    mov cx, 0ah
+    call Print_Number
+    
+    mov ah, 02h
+    mov dl, ','
+    int 21h
+    
+    mov sqrt_input_value, 00h
+    mov cx, 02h
+    
+RUN_SQRT_ALG_DECIMAL_PART:
+    ; DX:AX = Remainder
+    mov dx, aux_var_a[0]
+    mov ax, aux_var_a[2]
+    
+    ; DX:AX = DX:AX x 100
+    call Mul_By_100
+    
+    ; Remainder = DX:AX                    
+    mov aux_var_a[2], ax
+    mov aux_var_a[0], dx
+    
+    call Find_Sqrt_Next_Digit     ; BX = Next algarism
+    
+    ; Remainder = Remainder - Result
+    sub aux_var_a[2], ax
+    sbb aux_var_a[0], dx
+        
+    ; DX:AX = Result
+    mov ax, aux_var_b[02h]
+    mov dx, aux_var_b[00h]
+    
+    ; DX:AX = DX:AX x 10
+    call Mul_By_10
+    
+    ; DX:AX = DX:AX + BX
+    add ax, bx
+    adc dx, 00h
+    
+    ; Result = DX:AX
+    mov aux_var_b[2], ax
+    mov aux_var_b[0], dx
+    
+    mov ax, sqrt_input_value
+    mov dx, 0ah
+    mul dx
+    add ax, bx
+    mov sqrt_input_value, ax
+    
+    loop RUN_SQRT_ALG_DECIMAL_PART
+    
+    mov bx, sqrt_input_value
+    mov cx, 0ah
+    call Print_Number
+    
+    call Print_Line_Break
     
     ret
 Run_Sqrt_Alg endp
@@ -659,8 +988,8 @@ _begin:
     call Init_Segments
     
     ;call Run_Division_Alg 
-    
-    call Run_Sqrt_Alg
+                  
+    call Run_Sqrt_Alg 
       
     mov ah, 4ch
     int 21h
