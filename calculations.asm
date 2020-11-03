3a421d4071d794216097a1188c2dd48f2717bd96
vadimsZinatulins <vadims.zinatulins@pt.softinsa.com>
2020-11-03 14:26:09 +0000

    WIP: Input

--- calculations.asm
+++ calculations.asm
@@ -3,10 +3,13 @@
 .stack 100h
 
 .data
-    input_a     db  12 dup(00h)
-    input_b     db  12 dup(00h)
+    input_a     db  12 dup(00h), 00h
+    input_b     db  12 dup(00h), 00h
+                                    
+    input_str   db  12 dup(00h), 00h
     
-    
+    input_msg   db  "Introduza um numero: $"
+    mul_by_10_aux   db  12 dup(00h)
 .code
 
 Init_Segments proc
@@ -21,59 +24,158 @@ Init_Segments proc
 Init_Segments endp
 
 ; Input:
+;   DI -> Destination string
+;   SI -> Destination array
 ;   DL -> Base
-;   SI -> Address of the source array of bytes
 ; Output:
-;
-; Description:
-;   Prints the number in the array as ASCII in a specified base number.
-Print_Array proc
-    pusha 
+; Description
+Input_Value proc
+    pusha
+     
+    ; Clear Inputs                   
+    mov word ptr [di + 00h], 00h
+    mov word ptr [di + 02h], 00h
+    mov word ptr [di + 04h], 00h
+    mov word ptr [di + 06h], 00h
+    mov word ptr [di + 08h], 00h
+    mov word ptr [di + 0ah], 00h
     
-    mov ax, 00h
-    mov cx, 00h
-PRINT_ARRAY_LOOP:
-    mov ax, 00h
-    mov bx, cx
-    mov al, [si + bx]
+    mov word ptr [si + 00h], 00h
+    mov word ptr [si + 02h], 00h
+    mov word ptr [si + 04h], 00h
+    mov word ptr [si + 06h], 00h
+    mov word ptr [si + 08h], 00h
+    mov word ptr [si + 0ah], 00h
     
-    push ax
-    mov al, dl
-    call Div_By_Byte
-    pop ax
-    div dl
+    ; Input counter           
+    mov bx, 00h
+INPUT_VALUE_LOOP:
+    mov ah, 01h
+    int 21h   
     
-    ; If the remainder is above 9 then add 0x41h (to turn 
-    ; it into a alphabetical character in ASCII code)
-    cmp ah, 09h
-    ja PRINT_ARRAY_ADD_41H
+    ; Check if input is ENTER
+    cmp al, 0dh
+    je INPUT_VALUE_END
     
-    add ah, 30h
-    jmp PRINT_ARRAY_NEXT_LOOP_ITERATION: 
-
-PRINT_ARRAY_ADD_41H:
-    add ah, 41h
+    ; Check if input is BACKSPACE
+    cmp al, 08h
+    je INPUT_VALUE_BACKSPACE
     
-PRINT_ARRAY_NEXT_LOOP_ITERATION:
-    ; Store the ASCII value in the stack (in LSB of the word)
-    mov al, ah
+    ; Make sure input is no longer than 10 digits
+    cmp bx, 0ah
+    jae INPUT_VALUE_INVALID_INPUT 
+    
+    ; Check if it is a valid digit (0 - 1)
+    cmp al, 030h
+    jb INPUT_VALUE_INVALID_INPUT
+    cmp al, 03ah
+    jb INPUT_VALUE_IS_DIGIT
+    
+    ; Convert to lowercase
+    xor al, 040h
+    
+    ; Check if input is a valid character (A - F)
+    cmp al, 041h
+    jb INPUT_VALUE_INVALID_INPUT
+    cmp al, 47h
+    jb INPUT_VALUE_IS_CHAR 
+    
+    jmp INPUT_VALUE_INVALID_INPUT
+    
+INPUT_VALUE_INVALID_INPUT:
+    ; Store DX (because it will be used)
+    push dx
+    
+    ; Print BACKSPACE (to move cursor back)
     mov ah, 02h
+    mov dl, 08h
+    int 021h
+    
+    ; Increment BX (input counter) since it will be decremented
+    inc bx                                                     
+    
+    ; Restore DX
+    pop dx
+    
+INPUT_VALUE_BACKSPACE:
+    ; If BX (input counter) is 0 then don't decrement it 
+    cmp bx, 00h
+    je INPUT_VALUE_DONT_DEC_BX
+    
+    ; Decrement input counter
+    dec bx
+    
+INPUT_VALUE_DONT_DEC_BX:
+    ; Store DX (because it will be used)
+    push dx
+        
+    ; Print SPACE (to erase character from the screen)
+    mov ah, 02h
+    mov dl, 020h
+    int 021h
+    
+    ; Print BACKSPACE (to move cursor back)
+    mov dl, 08h
+    int 021h
+     
+    ; Restore DX 
+    pop dx
+    
+    jmp INPUT_VALUE_LOOP
+    
+INPUT_VALUE_IS_CHAR:
+    ; Input is a character (A - F) so remove 41h from it to get real value
+    sub al, 041h
+    jmp INPUT_VALUE_CONTINUE
+    
+INPUT_VALUE_IS_DIGIT:
+    ; Input is a number (0 - 9) so remove 30h from it to get real value
+    SUB al, 030h
+    
+INPUT_VALUE_CONTINUE:
+    ; If the real value of the input is greater than DL (base) then
+    ; it means that input value is invalid
+    cmp al, dl
+    jae INPUT_VALUE_INVALID_INPUT
+    
+    ; Store the input value
+    mov [di + bx], al
+    
+    ; Increment BX (input counter) 
+    inc bx
+    jmp INPUT_VALUE_LOOP    
+
+INPUT_VALUE_END:
+    
+    ; Store the number of inputs
+    mov [di + 0ch], bl
+    
+    mov cx, bx
+INPUT_VALUE_ACCUM_LOOP:
+    mov bx, 00h
+    mov bl, [di + 0xch]
+    sub bx, cx     
+    
+    mov al, [di + bx]
+    mov ah, 00h
+    
+    push di
     push ax
-    
-    inc cx
-    cmp cx, 0ah
-    jb PRINT_ARRAY_LOOP
-    
-    mov cx, 0bh
-PRINT_ARRAY_PRINT_LOOP:
+    mov ax, 00h
+    mov al, dl
+    mov di, si
+    call Mul_By_Byte
     pop ax
-    mov dl, al
-    int 21h
-    loop PRINT_ARRAY_PRINT_LOOP
+    
+    call Add_Word_To_Array
+    pop di
+    
+    loop INPUT_VALUE_ACCUM_LOOP
     
     popa
+    
     ret
-Print_Array endp
+Input_Value endp
 
 ; Inputs:
 ;   DI -> Address of the destination array of words
@@ -82,7 +184,7 @@ Print_Array endp
 ; Description:
 ;   [DI] = [DI] + [SI]
 Add_Array proc
-    pusha
+    pusha  
                  
     mov cx, 02h
     mov dx, [si]
@@ -111,7 +213,7 @@ Add_Array endp
 
 ; Input:
 ;   DI -> Address of the destination array of words
-;   AL -> Word to add
+;   AX -> Word to add
 ; Output:
 ; Description:
 ;   [DI] = [DI] + AX
@@ -194,33 +296,91 @@ Sub_Array endp
 
 ; Input:
 ;   DI -> Address of the destination array of words
-;   SI -> Address of the source array of words
+; Output:
+; Description:
+Mul_By_10 proc
+    pusha
+                           
+    mov ax, [di + 00h]
+    mov word ptr [mul_by_10_aux + 00h], ax
+    mov ax, [di + 02h]
+    mov word ptr [mul_by_10_aux + 02h], ax
+    mov ax, [di + 04h]
+    mov word ptr [mul_by_10_aux + 04h], ax
+    mov ax, [di + 06h]
+    mov word ptr [mul_by_10_aux + 06h], ax
+    mov ax, [di + 08h]
+    mov word ptr [mul_by_10_aux + 08h], ax
+    mov ax, [di + 0ah]
+    mov word ptr [mul_by_10_aux + 0ah], ax
+    
+    mov ax, 03h
+    call Rotate_Left_Array
+    
+    push di
+    lea di, mul_by_10_aux
+    mov ax, 01h
+    call Rotate_Left_Array
+    pop di
+    
+    lea si, mul_by_10_aux
+    call Add_Array   
+    
+    popa
+    ret
+Mul_By_10 endp
+
+; Input:
+;   DI -> Address of the destination array of words
 ;   AL -> Multiply value
 ; Output:
 ; Description:
 ;   [DI] = [SI] x AL
 Mul_By_Byte proc
     pusha
-                        
-    mov dx, [si + 00h]
-    mov [di + 00h], 00h
-    mov dx, [si + 02h]
-    mov [di + 02h], 00h
-    mov dx, [si + 04h]
-    mov [di + 04h], 00h
-    mov dx, [si + 06h]
-    mov [di + 06h], 00h
-    mov dx, [si + 08h]
-    mov [di + 08h], 00h
-    mov dx, [si + 0ah]
-    mov [di + 0ah], 00h
+    
+    ; Store copy of [DI] in stack                  
+    mov dx, [di + 0ah]
+    push dx
+    mov dx, [di + 08h]
+    push dx
+    mov dx, [di + 06h]
+    push dx
+    mov dx, [di + 04h]
+    push dx
+    mov dx, [di + 02h]
+    push dx
+    mov dx, [di + 00h]
+    push dx
+    
+    ; Clear input                  
+    mov word ptr [di + 0ah], 00h
+    mov word ptr [di + 08h], 00h
+    mov word ptr [di + 06h], 00h
+    mov word ptr [di + 04h], 00h
+    mov word ptr [di + 02h], 00h
+    mov word ptr [di + 00h], 00h
+    
+    mov bp, sp
     
     mov cx, 08h
 MUL_BY_BYTE_LOOP:    
     test al, 01h
     jz MUL_BY_BYTE_DO_SHIFT
     
-    call Add_Array
+    ; Add stack to [DI]
+    mov dx, [bp + 00h]
+    add [di + 00h], dx
+    mov dx, [bp + 02h]
+    adc [di + 02h], dx
+    mov dx, [bp + 04h]
+    adc [di + 04h], dx
+    mov dx, [bp + 06h]
+    adc [di + 06h], dx
+    mov dx, [bp + 08h]
+    adc [di + 08h], dx
+    mov dx, [bp + 0ah]
+    adc [di + 0ah], dx
         
 MUL_BY_BYTE_DO_SHIFT:
     mov dl, 00h
@@ -232,16 +392,14 @@ MUL_BY_BYTE_DO_SHIFT:
     call Rotate_Right_Array                       
     pop ax
     
-    adc dl, 00h
-    rol dl, 01h
-    
-    or [di + 0bh], dl
-    
     loop MUL_BY_BYTE_LOOP
     
     ; Shift result back           
     mov ax, 08h
     call Rotate_Left_Array
+    
+    ; Remove array copy from stack
+    add sp, 0ch
      
     popa
     ret
@@ -264,7 +422,7 @@ Div_By_Byte proc
     mov ah, 00h
     mov dx, 00h
     mov cx, 060h
-    mov cx, 08h
+    ;mov cx, 08h
     
 DIV_BY_BYTE_LOOP:
     shl dx, 01h
@@ -272,8 +430,8 @@ DIV_BY_BYTE_LOOP:
     mov ax, cx
     dec ax
     call Get_Bit_At
-    pop ax
     adc dx, 00h
+    pop ax
     
     cmp dx, ax
     jb DIV_BY_BYTE_CONTINUE_LOOP
@@ -377,22 +535,22 @@ Rotate_Right_Array proc
 ROTATE_RIGHT_ARRAY_LOOP:
     mov dl, 00h
     
-    shr [di + 00h], 01h
-    rcr [di + 01h], 01h
-    rcr [di + 02h], 01h
-    rcr [di + 03h], 01h
-    rcr [di + 04h], 01h
-    rcr [di + 05h], 01h
-    rcr [di + 06h], 01h
-    rcr [di + 07h], 01h
-    rcr [di + 08h], 01h
-    rcr [di + 09h], 01h
+    shr [di + 0bh], 01h
     rcr [di + 0ah], 01h
-    rcr [di + 0bh], 01h
+    rcr [di + 09h], 01h
+    rcr [di + 08h], 01h
+    rcr [di + 07h], 01h
+    rcr [di + 06h], 01h
+    rcr [di + 05h], 01h
+    rcr [di + 04h], 01h
+    rcr [di + 03h], 01h
+    rcr [di + 02h], 01h
+    rcr [di + 01h], 01h
+    rcr [di + 00h], 01h
     
     adc dl, 00h
     ror dl, 01h
-    or [di + 00h], dl
+    or [di + 0bh], dl
     
     loop ROTATE_RIGHT_ARRAY_LOOP
     
@@ -415,21 +573,21 @@ Rotate_Left_Array proc
 ROTATE_LEFT_ARRAY_LOOP:
     mov dl, 00h
     
-    shl [di + 0bh], 01h
-    rcl [di + 0ah], 01h
-    rcl [di + 09h], 01h
-    rcl [di + 08h], 01h
-    rcl [di + 07h], 01h
-    rcl [di + 06h], 01h
-    rcl [di + 05h], 01h
-    rcl [di + 04h], 01h
-    rcl [di + 03h], 01h
-    rcl [di + 02h], 01h
+    shl [di + 00h], 01h
     rcl [di + 01h], 01h
-    rcl [di + 00h], 01h
+    rcl [di + 02h], 01h
+    rcl [di + 03h], 01h
+    rcl [di + 04h], 01h
+    rcl [di + 05h], 01h
+    rcl [di + 06h], 01h
+    rcl [di + 07h], 01h
+    rcl [di + 08h], 01h
+    rcl [di + 09h], 01h
+    rcl [di + 0ah], 01h
+    rcl [di + 0bh], 01h
     
     adc dl, 00h
-    or [di + 0bh], dl
+    or [di + 00h], dl
     
     loop ROTATE_LEFT_ARRAY_LOOP
     
@@ -438,16 +596,16 @@ ROTATE_LEFT_ARRAY_LOOP:
 Rotate_Left_Array endp
 
 _begin:
-    call Init_Segments  
+    call Init_Segments      
+     
+    lea dx, input_msg
+    mov ah, 09h
+    int 021h
     
-    mov input_b[00h], 0ffh
-    
-    lea di, input_a
-    lea si, input_b 
-    
-    mov ax, 04h
-    
-    call Div_By_Byte
+    lea di, input_str
+    lea si, input_a 
+    mov dl, 0ah
+    call Input_Value
      
     mov ah, 0x4ch
     int 21h
